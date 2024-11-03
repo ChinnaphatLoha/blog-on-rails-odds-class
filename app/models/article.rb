34d58effe24d3b005c70bfa9483443a1b82fd2f1
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-11-03 14:33:18 +0700

    add status field to articles
    - and comments with validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,14 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
+  VALID_STATUSES = ["public", "private", "archived"]
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == "archived"
+  end
+
   def truncated_body
     ActionController::Base.helpers.truncate(body, length: 100)
   end
