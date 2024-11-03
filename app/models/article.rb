0a5fc54b773958c48ae95e0dfd6583a63eb14edf
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-11-03 13:51:24 +0700

    refactor article display with card styling
    - and add body truncation method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  def truncated_body
+    ActionController::Base.helpers.truncate(body, length: 100) # Adjust length as desired
+  end
 end
