a3736ec6c1cbdbe697fa482c653079d0eb5f561c
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-11-03 14:03:54 +0700

    add comments functionality
    - with controller, views, and routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,10 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
   def truncated_body
-    ActionController::Base.helpers.truncate(body, length: 100) # Adjust length as desired
+    ActionController::Base.helpers.truncate(body, length: 100)
   end
 end
